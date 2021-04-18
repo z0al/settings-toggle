d4e0cfacd61b44464b110f6633c0605b95ff2eb1
z0al <12673605+z0al@users.noreply.github.com>
2021-04-18 23:59:09 +0200

    refactor: extract and test getCurrentValue

---
+++ src/lib/getCurrentValue.ts
@@ -0,0 +1,26 @@
+// Ours
+import { isDefined } from './is';
+
+export function getCurrentValue(opts: {
+	target: 'global' | 'workspace';
+	defaultValue?: unknown;
+	globalValue?: unknown;
+	workspaceValue?: unknown;
+}) {
+	const hasGlobalValue = isDefined(opts.globalValue);
+	const hasDefaultValue = isDefined(opts.defaultValue);
+	const hasWorkspaceValue = isDefined(opts.workspaceValue);
+
+	const currentValue =
+		opts.target === 'workspace' && hasWorkspaceValue
+			? opts.workspaceValue
+			: hasGlobalValue
+			? opts.globalValue
+			: // Fallback to worksapceValue when the default value is
+			// undefined. Useful work things like `tasks`.
+			hasDefaultValue
+			? opts.defaultValue
+			: opts.workspaceValue;
+
+	return JSON.stringify(currentValue);
+}
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { commands } from './commands';
 import { ExtensionContext } from './types';
 
 export function activate(ctx: ExtensionContext) {
-	console.log(ctx.extension.id, 'activated');
-
 	// Register commands
 	commands.forEach((cmd) => {
 		ctx.subscriptions.push(Commands.register(cmd));
