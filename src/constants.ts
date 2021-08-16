5db4252d14dbe4bd543835c41199211e6410c6ef
z0al <12673605+z0al@users.noreply.github.com>
2021-08-16 15:11:48 +0200

    fix: adjust Quick Input title

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,7 @@
 import { Uri } from 'vscode';
 
 export const ExtensionId = 'settings-toggle';
+export const ExtensionName = 'Settings Toggle';
 
 export const SettingsSchemaUri = Uri.parse(
 	'vscode://schemas/settings/default'
